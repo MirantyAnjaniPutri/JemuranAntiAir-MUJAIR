a1fb4e2a00ab00f2c9623194aab423395957d0fc
MirantyAnjaniPutri <miranty.anjani@ui.ac.id>
2023-09-20 21:27:36 +0700

    First commit. Checking for URL.

---
+++ software_mujair/software_mujair.ino
@@ -0,0 +1,83 @@
+#include <ArduinoJson.h>
+#include <WiFi.h>
+
+const char* host = "api.open-meteo.com";  // Updated host to match your URL structure
+const int httpPort = 80;
+
+// Function to create the URL with custom latitude and longitude
+String createURL(double latitude, double longitude) {
+    // Base URL template
+    const char* baseURL = "/v1/forecast?latitude=%.4f&longitude=%.4f&hourly=temperature_2m,precipitation,rain,showers&timezone=Asia%%2FBangkok";
+
+    // Create a String object to store the URL
+    String url = String(baseURL);
+
+    // Format the URL with custom latitude and longitude
+    url.replace("%.4f", String(latitude));
+    url.replace("%.4f", String(longitude));
+
+    return url;
+}
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("Connecting to WiFi");
+    WiFi.begin("Wokwi-GUEST", "", 6);
+    
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(100);
+        Serial.println("...");
+    }
+    
+    Serial.println("Connected to WiFi");
+}
+
+void loop() {
+    double customLatitude, customLongitude;
+    
+    Serial.println("Enter Latitude:");
+    while (!Serial.available()) {}
+    customLatitude = Serial.parseFloat();
+    Serial.println(customLatitude);
+    
+    Serial.println("Enter Longitude:");
+    while (!Serial.available()) {}
+    customLongitude = Serial.parseFloat();
+    Serial.println(customLongitude);
+
+    String url = createURL(customLatitude, customLongitude);
+
+    WiFiClient client;
+
+    Serial.println("Connecting to ");
+    Serial.println(host);
+
+    if (!client.connect(host, httpPort)) {
+        Serial.println("Connection Failed");
+        return;
+    } else {
+        Serial.println("Connection Established Successfully");
+    }
+
+    Serial.println("Connecting to URL:");
+    Serial.println(url);
+
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
+
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+        if (millis() - timeout > 5000) {
+            Serial.println("Connection Timeout");
+            client.stop();
+            return;
+        }
+    }
+
+    while (client.available()) {
+        String line = client.readStringUntil('\r');
+        Serial.println(line);
+    }
+
+    Serial.println("Closing Connection");
+    client.stop();
+}
