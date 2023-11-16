60427d7398f7bf00b8dcad5a146690e5f54ff320
Vio-ES <viony.elizabeth@ui.ac.id>
2023-11-16 12:12:48 +0700

    rain, gps, and API

--- software_mujair/software_mujair.ino
+++ software_mujair/software_mujair.ino
@@ -4,6 +4,7 @@
 #include <WiFi.h>
 #include <Wire.h>
 #include <TinyGPSPlus.h>
+#include <ThingsBoard.h>
 
 const char* ssid = "v";
 const char* password = "vivita21";
@@ -16,6 +17,9 @@ double longitude;
 HardwareSerial neogps(1);
 TinyGPSPlus gps;
 
+#define RAIN_PIN 4
+bool rain_flag = false;
+
 struct WeatherData {
   String weatherMain;
   float temp;
@@ -25,12 +29,27 @@ struct WeatherData {
   float humidity;
 };
 
+void IRAM_ATTR rain_isr() {
+  // Read the rain sensor value
+  int rain_value = digitalRead(RAIN_PIN);
+
+  // Check if rain is detected
+  if (rain_value == LOW) {
+    // Set the flag to true
+    rain_flag = true;
+  }
+}
+
 void setup() {
+  pinMode(RAIN_PIN, INPUT);
+  
+  attachInterrupt(digitalPinToInterrupt(RAIN_PIN), rain_isr, FALLING);
+
   Serial.begin(115200);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
+    delay(10);
     Serial.println("Connecting to WiFi...");
   }
 
@@ -48,16 +67,25 @@ void setup() {
 void loop() {
   // Your code here
   boolean newData = false;
-  while(gps.location.lat() == 0 && gps.location.lng() == 0){
-    for (unsigned long start = millis(); millis() - start < 1000;)
-    {
-      while (neogps.available())
+
+  if(rain_flag == true) {
+
+    // Print a message to the serial monitor
+    Serial.println("Rain detected!");
+
+    // Reset the flag to false
+    rain_flag = false;
+  }else{
+    while(gps.location.lat() == 0 && gps.location.lng() == 0){
+      for (unsigned long start = millis(); millis() - start < 1000;)
       {
-        if (gps.encode(neogps.read()))
+        while (neogps.available())
         {
-          newData = true;
+          if (gps.encode(neogps.read()))
+          {
+            newData = true;
+          }
         }
-      }
     }
 
     //If newData is true
@@ -74,82 +102,84 @@ void loop() {
     Serial.println("Satelitte: " + String(gps.satellites.value()));
     Serial.println(gps.location.isValid());
     Serial.println(String(latitude) + " and " + String(longitude));
-  }
-
-  if(latitude == 0 && longitude == 0){
-    Serial.println("Enter Latitude (-180.000000 to 180.000000):");
-    while (!Serial.available()) {}  // Wait for user input
-    String latitudeInput = Serial.readStringUntil('\n');  // Read input as a string
-    latitude = latitudeInput.toDouble();
-    Serial.println(latitudeInput);
-    
-    // Validate latitude input
-    if (latitude < -90.0 || latitude > 90.0) {
-        Serial.println("Invalid latitude. Please enter a value between -90.000000 and 90.000000.");
-        return;  // Exit the loop if the latitude is out of range
     }
 
-    Serial.println("Enter Longitude (-180.000000 to 180.000000):");
-    while (!Serial.available()) {}  // Wait for user input
-    String longitudeInput = Serial.readStringUntil('\n');  // Read input as a string
-    longitude = longitudeInput.toDouble();
-    Serial.println(longitudeInput);
+    if(latitude == 0 && longitude == 0){
+      Serial.println("Enter Latitude (-180.000000 to 180.000000):");
+      while (!Serial.available()) {}  // Wait for user input
+      String latitudeInput = Serial.readStringUntil('\n');  // Read input as a string
+      latitude = latitudeInput.toDouble();
+      Serial.println(latitudeInput);
+      
+      // Validate latitude input
+      if (latitude < -90.0 || latitude > 90.0) {
+          Serial.println("Invalid latitude. Please enter a value between -90.000000 and 90.000000.");
+          return;  // Exit the loop if the latitude is out of range
+      }
 
-    // Validate longitude input
-    if (longitude < -180.0 || longitude > 180.0) {
-        Serial.println("Invalid longitude. Please enter a value between -180.000000 and 180.000000.");
-        return;  // Exit the loop if the longitude is out of range
+      Serial.println("Enter Longitude (-180.000000 to 180.000000):");
+      while (!Serial.available()) {}  // Wait for user input
+      String longitudeInput = Serial.readStringUntil('\n');  // Read input as a string
+      longitude = longitudeInput.toDouble();
+      Serial.println(longitudeInput);
+
+      // Validate longitude input
+      if (longitude < -180.0 || longitude > 180.0) {
+          Serial.println("Invalid longitude. Please enter a value between -180.000000 and 180.000000.");
+          return;  // Exit the loop if the longitude is out of range
+      }
     }
-  }
 
-  HTTPClient http;
-  String apiUrl = "http://api.openweathermap.org/data/2.5/weather?lat=" + String(latitude) + "&lon=" + String(longitude) + "&appid=" + apiKey;
-  http.begin(apiUrl);
+    HTTPClient http;
+    String apiUrl = "http://api.openweathermap.org/data/2.5/weather?lat=" + String(latitude) + "&lon=" + String(longitude) + "&appid=" + apiKey;
+    http.begin(apiUrl);
 
-  int httpResponseCode = http.GET();
+    int httpResponseCode = http.GET();
 
-  if (httpResponseCode > 0) {
-    String payload = http.getString();
+    if (httpResponseCode > 0) {
+      String payload = http.getString();
 
-    // Print the raw JSON response for debugging
-    Serial.println("Raw JSON response:");
-    Serial.println(payload);
+      // Print the raw JSON response for debugging
+      Serial.println("Raw JSON response:");
+      Serial.println(payload);
 
-    DynamicJsonDocument jsonDoc(2048); // Adjust the buffer size as needed
+      DynamicJsonDocument jsonDoc(2048); // Adjust the buffer size as needed
 
-    // Parse JSON
-    DeserializationError error = deserializeJson(jsonDoc, payload);
+      // Parse JSON
+      DeserializationError error = deserializeJson(jsonDoc, payload);
 
-    if (!error) {
-      WeatherData weather;
-      weather.weatherMain = jsonDoc["weather"][0]["main"].as<String>();
-      weather.temp = jsonDoc["main"]["temp"].as<float>();
-      weather.tempMin = jsonDoc["main"]["temp_min"].as<float>();
-      weather.tempMax = jsonDoc["main"]["temp_max"].as<float>();
-      weather.pressure = jsonDoc["main"]["pressure"].as<float>();
-      weather.humidity = jsonDoc["main"]["humidity"].as<float>();
+      if (!error) {
+        WeatherData weather;
+        weather.weatherMain = jsonDoc["weather"][0]["main"].as<String>();
+        weather.temp = jsonDoc["main"]["temp"].as<float>();
+        weather.tempMin = jsonDoc["main"]["temp_min"].as<float>();
+        weather.tempMax = jsonDoc["main"]["temp_max"].as<float>();
+        weather.pressure = jsonDoc["main"]["pressure"].as<float>();
+        weather.humidity = jsonDoc["main"]["humidity"].as<float>();
 
-      // Now you can use the 'weather' struct to access the data.
-      Serial.print("Weather Main: ");
-      Serial.println(weather.weatherMain);
-      Serial.print("Temperature: ");
-      Serial.println(weather.temp);
-      Serial.print("Temp Min: ");
-      Serial.println(weather.tempMin);
-      Serial.print("Temp Max: ");
-      Serial.println(weather.tempMax);
-      Serial.print("Pressure: ");
-      Serial.println(weather.pressure);
-      Serial.print("Humidity: ");
-      Serial.println(weather.humidity);
+        // Now you can use the 'weather' struct to access the data.
+        Serial.print("Weather Main: ");
+        Serial.println(weather.weatherMain);
+        Serial.print("Temperature: ");
+        Serial.println(weather.temp);
+        Serial.print("Temp Min: ");
+        Serial.println(weather.tempMin);
+        Serial.print("Temp Max: ");
+        Serial.println(weather.tempMax);
+        Serial.print("Pressure: ");
+        Serial.println(weather.pressure);
+        Serial.print("Humidity: ");
+        Serial.println(weather.humidity);
+      } else {
+        Serial.print("JSON Parsing Error: ");
+        Serial.println(error.c_str());
+      }
     } else {
-      Serial.print("JSON Parsing Error: ");
-      Serial.println(error.c_str());
+      Serial.print("HTTP error code: ");
+      Serial.println(httpResponseCode);
     }
-  } else {
-    Serial.print("HTTP error code: ");
-    Serial.println(httpResponseCode);
-  }
 
-  http.end();
+    http.end();
+    delay(1000);
+  }
 }
